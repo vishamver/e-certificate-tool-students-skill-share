20844037ca09901cc2d2cce3e76750616fbed92c
Vishamber <163025042+vishamver@users.noreply.github.com>
2025-01-01 16:54:59 +0530

    Update index.js
    Signed-off-by: Vishamber <163025042+vishamver@users.noreply.github.com>

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
 function check() {
     var input = document.getElementById('fname');
     if (input.value !== "") {
-        ecertificate();
-
-    }
-    else {
+        generateCertificate();
+    } else {
         alert("Input field is empty.");
     }
 }
-function ecertificate() {
+
+function generateCertificate() {
     var x = document.getElementById("main");
     x.style.display = "block";
     var name = document.getElementById('fname').value;
     var details = document.getElementById('fdetails').value;
     var oname = document.getElementById('forganisername').value;
-    document.getElementsByTagName('h3')[0].innerHTML = name;
-    document.getElementsByTagName('h4')[0].innerHTML = details;
-    document.getElementsByTagName('h5')[0].innerHTML = oname;
 
+    document.getElementById('name').innerText = name;
+    document.querySelector('.details').innerText = details;
+    document.querySelector('.organisername').innerText = oname;
 }
-function dcheck() {
-    var input = document.getElementById('fname');
-    if (input.value !== "") {
-        download();
 
-    }
-    else {
-        alert("Input field is empty.");
-    }
-}
-    function download() {
 
-        var body = document.getElementById('body').innerHTML;
-        var main = document.getElementById('main').innerHTML;
-        document.getElementById('body').innerHTML = main;
-        window.print();
-        document.getElementById('body').innerHTML = body;
 
-    }
-    
+
